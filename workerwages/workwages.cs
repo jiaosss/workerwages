f2fc9bd757cce838cf87b7c87eca22a5ee557bb5
越 焦 <jiaosss@outlook.com>
2022-10-28 18:33:13 +0800

    添加项目文件。

---
+++ workerwages/workwages.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace workerwages
+{
+    public partial class workwages
+    {
+        private void workwages_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
