afabb8fe41b50857f4c734361378f0d34e84d847
越 焦 <jiaosss@outlook.com>
2022-11-02 11:52:55 +0800

    v0.1.2测试版

--- workerwages/workwages.cs
+++ workerwages/workwages.cs
@@ -2,12 +2,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace workerwages
 {
     public partial class workwages
     {
         public string path = "";
+        public Excel.Application excelapp;
+       
         private void workwages_Load(object sender, RibbonUIEventArgs e)
         {
 
@@ -21,7 +25,8 @@ namespace workerwages
             }
             else
             {
-                this.compute_wages(path); //开始汇总表
+                //this.compute_wages(path); //开始汇总表
+                this.linshi(path);
             }
         }
 
@@ -34,25 +39,30 @@ namespace workerwages
 
         private void compute_wages(string path1) //开始汇总表
         {
-            Microsoft.Office.Interop.Excel.Application excelapp = new Microsoft.Office.Interop.Excel.Application();
-            //excelapp.Visible = false;
+           
             System.Windows.Forms.MessageBox.Show("正在计算");
-            //System.IO.DirectoryInfo folder = new System.IO.DirectoryInfo(path); //获取文件夹地址
+            System.IO.DirectoryInfo folder = new System.IO.DirectoryInfo(path); //获取文件夹地址
 
-            //foreach (System.IO.FileInfo file in folder.GetFiles("*.*")) //遍历文件夹excel文件
-            //{
-            //    System.Windows.Forms.MessageBox.Show(file.ToString());
+            foreach (System.IO.FileInfo file in folder.GetFiles("*.*")) //遍历文件夹excel文件
+            {
+                System.Windows.Forms.MessageBox.Show(file.ToString());
 
-            //}
-
-            //string file = "C: /Users/jiaos/Desktop/工人工资/已审/合正龙腾付款单 - 锴成建筑（非建行部分）-已审.xlsx";
-            excelapp = System.Runtime.InteropServices.Marshal.GetActiveObject("Excel.Application") as Microsoft.Office.Interop.Excel.Application;
-            //Microsoft.Office.Interop.Excel.Range rg;
-            (excelapp.Range["B2:C5"]).Select();
-
-
+            }
         }
 
+        private void linshi(string path1)
+        {
+            ////Microsoft.Office.Interop.Excel.Application excelapp = new Microsoft.Office.Interop.Excel.Application();
+            ////excelapp.Visible = false;
+            //excelapp = Globals.ThisAddIn.Application;
+            ////string file = "C: /Users/jiaos/Desktop/工人工资/已审/合正龙腾付款单 - 锴成建筑（非建行部分）-已审.xlsx";
+            ////Microsoft.Office.Interop.Excel.Worksheet ws = (Microsoft.Office.Interop.Excel.Worksheet)ThisWorkbook.Worksheets[1];
+
+            Excel.Workbook wbook = Globals.ThisAddIn.Application.ActiveWorkbook;  //当前活动workbook
+            Excel.Worksheet wsheet = (Excel.Worksheet)wbook.ActiveSheet;          //当前活动sheet 
+            //Excel.Range range = (Excel.Range)wsheet.Application.Selection;
+            wsheet.Range["A3:F6"].Select();
+        }
         private void editBox1_TextChanged_1(object sender, RibbonControlEventArgs e)
         {
             
