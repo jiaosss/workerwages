275a2d6a7e1df625638410112f89e5f592efa27c
越 焦 <jiaosss@outlook.com>
2023-01-26 19:20:10 +0800

    v1.2测试版 完成表格拆分功能

--- workerwages/workwages.Designer.cs
+++ workerwages/workwages.Designer.cs
@@ -37,22 +37,29 @@ namespace workerwages
         {
             this.tab1 = this.Factory.CreateRibbonTab();
             this.group1 = this.Factory.CreateRibbonGroup();
+            this.group3 = this.Factory.CreateRibbonGroup();
+            this.separator1 = this.Factory.CreateRibbonSeparator();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.label1 = this.Factory.CreateRibbonLabel();
+            this.label2 = this.Factory.CreateRibbonLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.button1 = this.Factory.CreateRibbonButton();
             this.button2 = this.Factory.CreateRibbonButton();
             this.findfile = this.Factory.CreateRibbonButton();
-            this.group2 = this.Factory.CreateRibbonGroup();
-            this.label1 = this.Factory.CreateRibbonLabel();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.splitexcel = this.Factory.CreateRibbonButton();
+            this.mergeexcel = this.Factory.CreateRibbonButton();
             this.tab1.SuspendLayout();
             this.group1.SuspendLayout();
+            this.group3.SuspendLayout();
             this.group2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tab1
             // 
             this.tab1.Groups.Add(this.group1);
+            this.tab1.Groups.Add(this.group3);
             this.tab1.Groups.Add(this.group2);
-            this.tab1.Label = "工人工资计算";
+            this.tab1.Label = "澄澄的工具箱";
             this.tab1.Name = "tab1";
             // 
             // group1
@@ -60,7 +67,36 @@ namespace workerwages
             this.group1.Items.Add(this.button1);
             this.group1.Items.Add(this.button2);
             this.group1.Items.Add(this.findfile);
+            this.group1.Label = "工人账号信息核对";
             this.group1.Name = "group1";
+            // 
+            // group3
+            // 
+            this.group3.Items.Add(this.splitexcel);
+            this.group3.Items.Add(this.separator1);
+            this.group3.Items.Add(this.mergeexcel);
+            this.group3.Label = "表格操作";
+            this.group3.Name = "group3";
+            // 
+            // separator1
+            // 
+            this.separator1.Name = "separator1";
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.label1);
+            this.group2.Items.Add(this.label2);
+            this.group2.Name = "group2";
+            // 
+            // label1
+            // 
+            this.label1.Label = "v1.2测试版";
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Label = "表格合并测试";
+            this.label2.Name = "label2";
             // 
             // button1
             // 
@@ -76,7 +112,7 @@ namespace workerwages
             this.button2.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
             this.button2.Label = "选择信息表";
             this.button2.Name = "button2";
-            this.button2.OfficeImageId = "OpenAppointment";
+            this.button2.OfficeImageId = "FileNewDocument";
             this.button2.ShowImage = true;
             this.button2.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button2_Click);
             // 
@@ -89,15 +125,22 @@ namespace workerwages
             this.findfile.ShowImage = true;
             this.findfile.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.findfile_Click);
             // 
-            // group2
+            // splitexcel
             // 
-            this.group2.Items.Add(this.label1);
-            this.group2.Name = "group2";
+            this.splitexcel.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.splitexcel.Label = "拆分表格";
+            this.splitexcel.Name = "splitexcel";
+            this.splitexcel.OfficeImageId = "ContactCardCopy";
+            this.splitexcel.ShowImage = true;
+            this.splitexcel.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.splitexcel_Click);
             // 
-            // label1
+            // mergeexcel
             // 
-            this.label1.Label = "v0.5.test";
-            this.label1.Name = "label1";
+            this.mergeexcel.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.mergeexcel.Label = "合并表格";
+            this.mergeexcel.Name = "mergeexcel";
+            this.mergeexcel.OfficeImageId = "GroupAdpQueryType";
+            this.mergeexcel.ShowImage = true;
             // 
             // workwages
             // 
@@ -109,6 +152,8 @@ namespace workerwages
             this.tab1.PerformLayout();
             this.group1.ResumeLayout(false);
             this.group1.PerformLayout();
+            this.group3.ResumeLayout(false);
+            this.group3.PerformLayout();
             this.group2.ResumeLayout(false);
             this.group2.PerformLayout();
             this.ResumeLayout(false);
@@ -125,6 +170,11 @@ namespace workerwages
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
         internal Microsoft.Office.Tools.Ribbon.RibbonLabel label1;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel label2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton splitexcel;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton mergeexcel;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator1;
     }
 
     partial class ThisRibbonCollection
--- workerwages/workwages.cs
+++ workerwages/workwages.cs
@@ -15,10 +15,11 @@ namespace workerwages
         public string path1 = "";  //分表文件路径
         public string path2 = "";  //信息表路径
         public Excel.Application excelapp;
-        public int number_excel = 1;
-        public int number_raw = 1;
+        public int number_excel = 1; //分表序号
+        public int number_raw = 1;   //汇总表行号
         public int number_column = 1;
-       
+        public int number_newinformation = 1; //信息表新增序号
+
         private void workwages_Load(object sender, RibbonUIEventArgs e)
         {
 
@@ -26,11 +27,11 @@ namespace workerwages
 
         private void findfile_Click(object sender, RibbonControlEventArgs e) //计算按钮
         {
-            if (path.Length == 0)
+            if (path == string.Empty)
             {
                 MessageBox.Show("请选择文件夹路径");
             }
-            else if (path2.Length == 0)
+            else if (path2 == string.Empty)
             {
                 MessageBox.Show("请选择信息表路径");
             }
@@ -73,6 +74,13 @@ namespace workerwages
             Excel.Worksheet wsheet = (Excel.Worksheet)wbook.ActiveSheet;          //当前活动sheet
 
 
+            //提前处理信息表
+            Excel.Application xlinformation = new Excel.Application();
+            Excel.Workbook inbook = xlinformation.Workbooks.Open(path2);
+            Excel.Worksheet insheet1 = inbook.Sheets[1];
+            Excel.Worksheet insheet2 = inbook.Sheets[2];
+            Excel.Range inrng = xlinformation.Range["A65535"].End[Excel.XlDirection.xlUp];
+
             foreach (System.IO.FileInfo file in folder.GetFiles("*.*")) //遍历文件夹excel文件
             {
                 //System.IO.FileInfo file = folder.GetFiles("*.*")[4];                                                //调试用，用完删除并解除循环注释
@@ -103,8 +111,9 @@ namespace workerwages
                 //汇总
                 if (number_excel == 1)
                 {
+                    //复制表头
+
                     wsheet.Range["A1:M1"].NumberFormat = "@";   //设置格式
-                    //((Excel.Range)xlWorkSheet.Cells[i, j]).HorizontalAlignment = Excel.XlVAlign.xlVAlignCenter;
                     wsheet.Range["B1"].Value2 = xlworksheet.Range["B1"].Value2;     //复制表头
                     wsheet.Range["A1"].Value2 = xlworksheet.Range["C1"].Value2;
                     wsheet.Range["C1:H1"].Value2 = xlworksheet.Range["D1:I1"].Value2;
@@ -113,6 +122,9 @@ namespace workerwages
 
                     number_excel = number_excel + 1;
                     number_raw = number_raw + 1;
+
+
+                    //复制第一个表的数据
 
                     wsheet.Range["A" + number_raw.ToString() + ":M" + (rng.Row + number_raw - 1).ToString()].NumberFormat = "@";
                     wsheet.Range["A" + number_raw.ToString()].Value2 = file.ToString();   //复制文件名
@@ -123,6 +135,29 @@ namespace workerwages
                     wsheet.Range["B" + (number_raw + 1).ToString() + ":B" + (number_raw + rng.Row).ToString()].Value2 = xlworksheet.Range["B2:B" + rng.Row.ToString()].Value2;     //复制表头
                     wsheet.Range["A" + (number_raw + 1).ToString() + ":A" + (number_raw + rng.Row).ToString()].Value2 = xlworksheet.Range["C2:C" + rng.Row.ToString()].Value2;
                     wsheet.Range["C" + (number_raw + 1).ToString() + ":H" + (number_raw + rng.Row).ToString()].Value2 = xlworksheet.Range["D2:I" + rng.Row.ToString()].Value2;
+
+                    //验证数据
+                    for (int i = 1; i < rng.Row ; i++)
+                    {
+                        string j = Convert.ToString((wsheet.Range["H" + (number_raw + i).ToString()].Value2));
+                        Excel.Range ranfind = insheet1.Range["H:H"].Find(j);
+
+                        if (ranfind != null)
+                        {
+                            wsheet.Range["J" + (number_raw + i).ToString()].Value2 = wsheet.Range["B" + (number_raw + i).ToString()].Value2;
+                        }
+                        else
+                        {
+                            insheet1.Range["A" + (inrng.Row + number_newinformation).ToString() + ":I" + (inrng.Row + number_newinformation).ToString()].NumberFormat = "@";
+                            insheet1.Range["A" + (inrng.Row + number_newinformation).ToString() + ":H" + (inrng.Row + number_newinformation).ToString()].Value2 = wsheet.Range["A" + (number_raw + i).ToString() + ":H" + (number_raw + i).ToString()].Value2;
+                            insheet1.Range["I" + (inrng.Row + number_newinformation).ToString()].Value2 = (DateTime.Now.ToString("yyyy.MM.dd") + "新增");
+                            insheet1.Range["A" + (inrng.Row + number_newinformation).ToString() + ":I" + (inrng.Row + number_newinformation).ToString()].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter; //单元格横向居中
+                            insheet1.Range["A" + (inrng.Row + number_newinformation).ToString() + ":I" + (inrng.Row + number_newinformation).ToString()].VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;   //单元格竖向居中
+                            wsheet.Range["J" + (number_raw + i)].Value2 = "新增";
+                            number_newinformation = number_newinformation + 1;
+                        }
+                        
+                    }
 
                     number_raw = number_raw + rng.Row + 1;
                 }
@@ -137,6 +172,29 @@ namespace workerwages
                     wsheet.Range["B" + ( number_raw + 1 ).ToString() + ":B" + ( number_raw + rng.Row ).ToString()].Value2 = xlworksheet.Range["B2:B" + rng.Row.ToString()].Value2;     //复制表头
                     wsheet.Range["A" + ( number_raw + 1 ).ToString() + ":A" + ( number_raw + rng.Row ).ToString()].Value2 = xlworksheet.Range["C2:C" + rng.Row.ToString()].Value2;
                     wsheet.Range["C" + ( number_raw + 1 ).ToString() + ":H" + ( number_raw + rng.Row ).ToString()].Value2 = xlworksheet.Range["D2:I" + rng.Row.ToString()].Value2;
+
+                    //验证数据
+                    for (int i = 1; i < rng.Row; i++)
+                    {
+                        string j = Convert.ToString((wsheet.Range["H" + (number_raw + i).ToString()].Value2));
+                        Excel.Range ranfind = insheet1.Range["H:H"].Find(j);
+
+                        if (ranfind != null)
+                        {
+                            wsheet.Range["J" + (number_raw + i).ToString()].Value2 = wsheet.Range["B" + (number_raw + i).ToString()].Value2;
+                        }
+                        else
+                        {
+                            insheet1.Range["A" + (inrng.Row + number_newinformation).ToString() + ":I" + (inrng.Row + number_newinformation).ToString()].NumberFormat = "@";
+                            insheet1.Range["A" + (inrng.Row + number_newinformation).ToString() + ":H" + (inrng.Row + number_newinformation).ToString()].Value2 = wsheet.Range["A" + (number_raw + i).ToString() + ":H" + (number_raw + i).ToString()].Value2;
+                            insheet1.Range["I" + (inrng.Row + number_newinformation).ToString()].Value2 = (DateTime.Now.ToString("yyyy.MM.dd") + "新增");
+                            insheet1.Range["A" + (inrng.Row + number_newinformation).ToString() + ":I" + (inrng.Row + number_newinformation).ToString()].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter; //单元格横向居中
+                            insheet1.Range["A" + (inrng.Row + number_newinformation).ToString() + ":I" + (inrng.Row + number_newinformation).ToString()].VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;   //单元格竖向居中
+                            wsheet.Range["J" + (number_raw + i)].Value2 = "新增";
+                            number_newinformation = number_newinformation + 1;
+                        }
+
+                    }
 
                     number_raw = number_raw + rng.Row + 1;
 
@@ -162,18 +220,149 @@ namespace workerwages
             wsheet.Range["A1:L" + number_raw.ToString()].VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;   //单元格竖向居中
 
             //保存汇总表
-            new_xlapp.ActiveWorkbook.SaveAs(path + "\\汇总表.xlsx");
+            new_xlapp.ActiveWorkbook.SaveAs(path + "\\汇总表" + DateTime.Now.ToString("yyyy.MM.dd") + ".xlsx"); //保存汇总表
+            xlinformation.ActiveWorkbook.SaveAs(path + "\\信息表" + DateTime.Now.ToString("yyyy.MM.dd") + ".xlsx");
+
 
             //关闭当前工作簿
             new_xlapp.ActiveWorkbook.Close(false);
-            
+            xlinformation.ActiveWorkbook.Close(false);
+
             //杀掉当前进程
             PublicMethod.Kill(new_xlapp);
+            PublicMethod.Kill(xlinformation);
+
+        }
+
+        private void splitexcel_Click(object sender, RibbonControlEventArgs e)
+        {
+            
+            //获取当前excel文件
+            Excel.Application new_xlapp = Globals.ThisAddIn.Application;
+            Excel.Workbook wbook = Globals.ThisAddIn.Application.ActiveWorkbook;  //当前活动workbook
+            Excel.Worksheet wsheet = (Excel.Worksheet)wbook.ActiveSheet;          //当前活动sheet
+
+            
+            string sc = JudgmentExcelColumn(wsheet);//判断最后非空列
+            int hc = JudgmentExcelRaw(wsheet);   //判断最后非空行(数值)
+
+
+            //删除A列重复值，做为拆分依据
+            string[] Adata = new string[hc];
+            for (int h = 0; h < hc; h++)
+            {
+                Adata[h] = Convert.ToString( wsheet.Range["A" + (h+1).ToString()].Value2);
+            }
+            string[] AdataNoDvalues = Adata.Distinct().ToArray(); //删除重复值
+
+
+            //开始拆分
+            MessageBox.Show("请选择保存拆分表格路径");
+            this.folderBrowserDialog1.ShowDialog();
+            path = this.folderBrowserDialog1.SelectedPath;
+            for (int i = 1; i < AdataNoDvalues.Length; i++)
+            {
+                Excel.Application splitexcel = new Excel.Application();
+                Excel.Workbook inbook = splitexcel.Workbooks.Add();
+                Excel.Worksheet insheet1 = inbook.Sheets[1];
+                insheet1.Range["A1:" + sc + "1"].Value2 = wsheet.Range["A1:" + sc + "1"].Value2;
+                int n = 2;
+                for (int j = 1;j < hc;j++)
+                {
+                    
+                    if (AdataNoDvalues[i] == Convert.ToString(wsheet.Range["A" + (j + 1).ToString()].Value2))
+                    {
+                        insheet1.Range["A" + n.ToString() + ":" + sc + n.ToString()].NumberFormat = "@";
+                        insheet1.Range["A" + n.ToString() + ":" + sc + n.ToString()].Value2 = wsheet.Range["A" + (j + 1).ToString() + ":" + sc +(j + 1).ToString()].Value2;
+                        n = n + 1;
+                    }
+                }
+
+                //设置自动列宽
+                insheet1.Columns.EntireColumn.AutoFit();
+
+                //单元格居中
+                insheet1.Range["A1:" + sc + hc.ToString()].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter; //单元格横向居中
+                insheet1.Range["A1:" + sc + hc.ToString()].VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;   //单元格竖向居中
+
+                //保存拆分表
+                splitexcel.ActiveWorkbook.SaveAs(path + "\\" + AdataNoDvalues[i] + ".xlsx"); //保存汇总表
+
+                //关闭当前工作簿
+                splitexcel.ActiveWorkbook.Close(false);
+
+                //杀掉当前进程
+                PublicMethod.Kill(splitexcel);
+
+            }
+            MessageBox.Show("拆分完成");
 
         }
 
 
+        public int JudgmentExcelRaw(Excel.Worksheet fristsheet)  //判断最后非空行
+        {
+            Excel.Worksheet FristSheet = fristsheet;
+            int RowsCount = FristSheet.UsedRange.Cells.Rows.Count;
+            return RowsCount;
+        }
+
+
+
+
+        public string JudgmentExcelColumn(Excel.Worksheet fristsheet)  //判断最后非空列
+        {
+            string[] ColumnName = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            int SecondColumnCount = 676;
+            string[] ColumnNames = new string[SecondColumnCount];
+
+            for (int i = 0; i < ColumnName.Length; i++)
+            {
+                ColumnNames[i] = ColumnName[i];
+            }
+
+            int n = 26;
+            for (int i = 0; i < ColumnName.Length; i++)
+            {
+                for (int j = 0; j < ColumnName.Length; j++)
+                {
+                    if (n + j < SecondColumnCount)
+                    {
+                        ColumnNames[n] = ColumnName[i] + ColumnName[j];
+                        n = n + 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (ColumnNames[SecondColumnCount - 1] != null)
+                {
+                    break;
+                }
+
+            }
+
+
+            //指定要操作的Sheet
+            Excel.Worksheet FristSheet = fristsheet;
+
+            //获取该张表的总行数
+            //int RowsCount = FristSheet.UsedRange.Cells.Rows.Count;
+            //获取该的总列数
+            int ColumnCount = FristSheet.UsedRange.Cells.Columns.Count;
+
+            //根据所获取的最大列数,获取到该列号名称
+            //string sc = ColumnNames[ColumnCount - 1].ToString() + RowsCount.ToString();
+            
+
+            return ColumnNames[ColumnCount - 1].ToString();
+        }
+
+
     }
+
+
 
 
     public class PublicMethod      //杀死进程
